9aa759e07e2a9bb495336da236a323cefe14414c
SYEDA ARBISH <jugnoo708@gmail.com>
2024-07-04 17:33:52 +0500

    Add files via upload

---
+++ TEMPERATURE CONVERTER/PYTHON/TEM.PY
@@ -0,0 +1,6 @@
+temp=float(input("enter the temperature in celsius :"))
+f=59*temp+32
+print(temp,"celsius is equal to",f,"fahrenheight")
+temp=float(input("enter the temperature in fahrenheight :"))
+c=95*(temp-32)
+print(temp,"fahrenheight is convert",c,"celsius")
